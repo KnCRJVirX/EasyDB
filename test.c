f68efe4dbf5f84551d6b046afc40300ad6cd14c5
KnCRJNET <kncrjvirx@gmail.com>
2025-01-07 00:47:43 +0800

    v0.02	在test.c中添加了删除的测试

--- test.c
+++ test.c
@@ -7,46 +7,68 @@ int main(int argc, char const *argv[])
     char* columnNames[] = {"ID", "Name", "Balance"};
     edbCreate(".\\test.db", 3, dataType, dataLens, 1, columnNames);
     EasyDB db;
-    edbOpen(".\\test.db", &db);
-    printf("%s\t%s\t%s\n", db.columnNames[0], db.columnNames[1], db.columnNames[2]);
 
-    edb_int newID = 1;
+    edbOpen(".\\test.db", &db);
+    printf("%s\t\t%s\t%s\n", db.columnNames[0], db.columnNames[1], db.columnNames[2]);
+
+    edb_int newID = 55240888;                              //插入第一项测试数据
     char* newName = "Alice";
     double newBalance = 520.1314;
     void* newLine[] = {&newID, newName, &newBalance};
     edbInsert(&db, newLine);
 
     EDBRow* ptr;
-    for (ptr = db.head->next; ptr != db.tail; ptr = ptr->next)
+    for (ptr = db.head->next; ptr != db.tail; ptr = ptr->next)      //遍历打印测试数据
     {
         printf("%d\t%s\t%lf\n", *(edb_int*)(ptr->data[0]), ptr->data[1], *(double*)(ptr->data[2]));
     }
     printf("\n");
-    edbClose(&db);
+    edbClose(&db);                  //关闭数据库
 
-    edbOpen(".\\test.db", &db);
-    printf("%s\t%s\t%s\n", db.columnNames[0], db.columnNames[1], db.columnNames[2]);
+    edbOpen(".\\test.db", &db);     //重新打开，以测试edbClose和edbOpen
 
-    for (ptr = db.head->next; ptr != db.tail; ptr = ptr->next)
+    printf("%s\t\t%s\t%s\n", db.columnNames[0], db.columnNames[1], db.columnNames[2]);
+    for (ptr = db.head->next; ptr != db.tail; ptr = ptr->next)      //遍历打印测试数据
     {
         printf("%d\t%s\t%lf\n", *(edb_int*)(ptr->data[0]), ptr->data[1], *(double*)(ptr->data[2]));
     }
     printf("\n");
 
-    newID = 2;
+    newID = 55230777;                                  //插入第二项测试数据
     newLine[1] = "Bob";
     newBalance = 114514.1919810;
     edbInsert(&db, newLine);
     edbClose(&db);
 
     edbOpen(".\\test.db", &db);
-    printf("%s\t%s\t%s\n", db.columnNames[0], db.columnNames[1], db.columnNames[2]);
 
-    for (ptr = db.head->next; ptr != db.tail; ptr = ptr->next)
+    printf("%s\t\t%s\t%s\n", db.columnNames[0], db.columnNames[1], db.columnNames[2]);
+    for (ptr = db.head->next; ptr != db.tail; ptr = ptr->next)      //遍历打印测试数据
     {
         printf("%d\t%s\t%lf\n", *(edb_int*)(ptr->data[0]), ptr->data[1], *(double*)(ptr->data[2]));
     }
     printf("\n");
+
+    ptr = db.head->next;                            //删除第一项测试数据
+    edbDelete(&db, ptr);
+
+    printf("%s\t\t%s\t%s\n", db.columnNames[0], db.columnNames[1], db.columnNames[2]);
+    for (ptr = db.head->next; ptr != db.tail; ptr = ptr->next)      //遍历打印测试数据
+    {
+        printf("%d\t%s\t%lf\n", *(edb_int*)(ptr->data[0]), ptr->data[1], *(double*)(ptr->data[2]));
+    }
+    printf("\n");
+    edbClose(&db);                  //关闭数据库
+
+    edbOpen(".\\test.db", &db);     //重新打开，以测试edbClose和edbOpen
+
+    printf("%s\t\t%s\t%s\n", db.columnNames[0], db.columnNames[1], db.columnNames[2]);
+    for (ptr = db.head->next; ptr != db.tail; ptr = ptr->next)      //遍历打印测试数据
+    {
+        printf("%d\t%s\t%lf\n", *(edb_int*)(ptr->data[0]), ptr->data[1], *(double*)(ptr->data[2]));
+    }
+    printf("\n");
+
     edbClose(&db);
     
     return 0;
